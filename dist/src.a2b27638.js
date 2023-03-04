2d072373cce3596bd3b0f1c807cc7cdfd969f7d2
ueno motoki <m.u19953001@gmail.com>
2023-03-04 04:02:14 +0000

    Reactでよく使われるJavascript

---
+++ dist/src.a2b27638.js
@@ -0,0 +1,422 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"src/index.js":[function(require,module,exports) {
+// // ほぼconst
+// // const変更不可だけどオブジェクトと配列は変更可能だしpushメソッドで追加もできる！
+
+// const func1 = (str) => {
+//   return str;
+// };
+// console.log(func1("aiueo"));
+
+// const func2 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func2(10, 10));
+
+// // 一列だと省略できる
+// const func3 = (num1, num2) => num1 * num2;
+// console.log(func3(10, 100));
+
+// //オブジェクトプロパティがあってれば順番関係なし
+// const myProfile = {
+//   name: "ueno",
+//   age: 28,
+// };
+
+// // 分割代入することでいちいちmyProfile.nameと書かなくても良くなる
+// const { name, age } = myProfile;
+// const message1 = `名前は${name}です！年齢は${age}さいです。`;
+// console.log(message1);
+
+// // 配列順番重視
+// const myProfile2 = ["ueno", 28];
+
+// const message2 = `名前は${myProfile2[0]}です。年齢は${myProfile2[1]}歳です！`;
+// console.log(message2);
+
+// // デフォルト値,引数
+
+// const sayHello = (name = "名無し") => {
+//   console.log(`やあ！${name}さん！`);
+// };
+// sayHello("上野");
+// // デフォルト値でナナシと入れているため名無しさんと入る
+// sayHello();
+
+// スプレッド構文
+// 配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// // 配列をコピーしてくれてる
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => {
+//   console.log(num1 + num2);
+// };
+// sumFunc(arr1[0], arr1[1]);
+// // 結果は一緒
+// sumFunc(...arr1);
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...num3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(...arr2);
+
+// const arr3 = [10, 20];
+// const arr4 = [30, 40];
+// // 配列コピー！
+// const arr5 = [...arr3];
+// console.log(arr5);
+
+// const arr6 = [...arr3, ...arr4];
+// console.log(arr6);
+// // false↓
+// console.log(arr3 === arr5);
+
+// mapやfilterを使った配列の処理 for文はあんまり使われなくなった
+
+// const nameArr = ["田中", "山田", "上野", "追加名前"];
+
+// // const nameArr2 = nameArr.map((name) => {
+// //   return name;
+// // });
+// // console.log(nameArr2);
+
+// // 番号付き
+// // nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です。`));
+
+// // // filterは配列から必要なものだけ参照できる↓
+
+// // const numArr = [1, 2, 3, 4, 5];
+// // const newNumArr = numArr.filter((num) => {
+// //   return num % 2 === 0;
+// // });
+
+// // // 偶数だけ参照できる
+// // console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   // if (name === "上野") {
+//   //   return name;
+//   // } else {
+//   //   return `${name}さん`;
+//   // }
+//   {
+//     return name === "上野" ? name : `${name}さん`;
+//   }
+// });
+
+// console.log(newNameArr);
+
+// 三項演算子
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100超えてるよ" : "100以下です許容範囲です";
+// };
+
+// console.log(checkSum(100, 50));
+// console.log(checkSum(10, 50));
+
+//  ||は左側がfalseになる時右側を返す
+var num = 100;
+var fee = num || "金額未設定です";
+console.log(fee); //100が返る
+
+// 　&&は左がtrueなら右側を返す
+var num2 = 0;
+var num3 = 100;
+var fee2 = num2 && "trueだよ";
+var fee3 = num3 && "trueだよ";
+console.log(fee2); //0はfalseなのでそのままの値が返る
+console.log(fee3); //trueなので右側を返す
+},{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37029" + '/');
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      });
+
+      // Enable HMR for CSS by default.
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+  if (overlay) {
+    overlay.remove();
+  }
+}
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID;
+
+  // html encode message and stack trace
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+  var parents = [];
+  var k, d, dep;
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+  return parents;
+}
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+  if (checkedAssets[id]) {
+    return;
+  }
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+    return true;
+  }
+}
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","src/index.js"], null)
+//# sourceMappingURL=/src.a2b27638.js.map
--- src/index.js
+++ src/index.js
@@ -1,10 +1,131 @@
-import "./styles.css";
+// // ほぼconst
+// // const変更不可だけどオブジェクトと配列は変更可能だしpushメソッドで追加もできる！
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// const func1 = (str) => {
+//   return str;
+// };
+// console.log(func1("aiueo"));
+
+// const func2 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func2(10, 10));
+
+// // 一列だと省略できる
+// const func3 = (num1, num2) => num1 * num2;
+// console.log(func3(10, 100));
+
+// //オブジェクトプロパティがあってれば順番関係なし
+// const myProfile = {
+//   name: "ueno",
+//   age: 28,
+// };
+
+// // 分割代入することでいちいちmyProfile.nameと書かなくても良くなる
+// const { name, age } = myProfile;
+// const message1 = `名前は${name}です！年齢は${age}さいです。`;
+// console.log(message1);
+
+// // 配列順番重視
+// const myProfile2 = ["ueno", 28];
+
+// const message2 = `名前は${myProfile2[0]}です。年齢は${myProfile2[1]}歳です！`;
+// console.log(message2);
+
+// // デフォルト値,引数
+
+// const sayHello = (name = "名無し") => {
+//   console.log(`やあ！${name}さん！`);
+// };
+// sayHello("上野");
+// // デフォルト値でナナシと入れているため名無しさんと入る
+// sayHello();
+
+// スプレッド構文
+// 配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// // 配列をコピーしてくれてる
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => {
+//   console.log(num1 + num2);
+// };
+// sumFunc(arr1[0], arr1[1]);
+// // 結果は一緒
+// sumFunc(...arr1);
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...num3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(...arr2);
+
+// const arr3 = [10, 20];
+// const arr4 = [30, 40];
+// // 配列コピー！
+// const arr5 = [...arr3];
+// console.log(arr5);
+
+// const arr6 = [...arr3, ...arr4];
+// console.log(arr6);
+// // false↓
+// console.log(arr3 === arr5);
+
+// mapやfilterを使った配列の処理 for文はあんまり使われなくなった
+
+// const nameArr = ["田中", "山田", "上野", "追加名前"];
+
+// // const nameArr2 = nameArr.map((name) => {
+// //   return name;
+// // });
+// // console.log(nameArr2);
+
+// // 番号付き
+// // nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です。`));
+
+// // // filterは配列から必要なものだけ参照できる↓
+
+// // const numArr = [1, 2, 3, 4, 5];
+// // const newNumArr = numArr.filter((num) => {
+// //   return num % 2 === 0;
+// // });
+
+// // // 偶数だけ参照できる
+// // console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   // if (name === "上野") {
+//   //   return name;
+//   // } else {
+//   //   return `${name}さん`;
+//   // }
+//   {
+//     return name === "上野" ? name : `${name}さん`;
+//   }
+// });
+
+// console.log(newNameArr);
+
+// 三項演算子
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100超えてるよ" : "100以下です許容範囲です";
+// };
+
+// console.log(checkSum(100, 50));
+// console.log(checkSum(10, 50));
+
+//  ||は左側がfalseになる時右側を返す
+const num = 100;
+const fee = num || "金額未設定です";
+console.log(fee); //100が返る
+
+// 　&&は左がtrueなら右側を返す
+const num2 = 0;
+const num3 = 100;
+const fee2 = num2 && "trueだよ";
+const fee3 = num3 && "trueだよ";
+console.log(fee2); //0はfalseなのでそのままの値が返る
+console.log(fee3); //trueなので右側を返す
